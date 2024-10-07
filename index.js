b87089a5f332807b2912fb862589dc731c77fefb
akshitshukrawal <akshitshukrawal@gmail.com>
2024-10-07 19:21:26 +0530

    changes

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ const app = express();
 // Middleware
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://event-management-akshit.vercel.app"],
+        method:["POST","GET"],
+        Credential:true
+    }
+));
 
 // MongoDB connection
 const mongoURI = 'mongodb+srv://akshitshukrawal:nUiGNtwDY8pIakae@megamind.v32mq.mongodb.net/?retryWrites=true&w=majority&appName=Megamind';
